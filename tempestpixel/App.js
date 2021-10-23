82cb223c0eec84a62473292e44e0bb83c8e12cab
Atabeso Bright <78513607+TempestLog@users.noreply.github.com>
2021-10-23 01:46:22 +0000

    update

---
+++ tempestpixel/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textStyles}>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#009900',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textstyles: {
+    color: 'red',
+    fontSize: 100
+  }
+});
