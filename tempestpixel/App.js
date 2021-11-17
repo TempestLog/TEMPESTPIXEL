6a8d9b41902bd400e423375f83dfc0885aa8b1e1
Atabeso Bright <78513607+TempestLog@users.noreply.github.com>
2021-11-17 23:31:06 +0000

    update

--- tempestpixel/App.js
+++ tempestpixel/App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Login from './screens/login';
+import Home from './screens/home';
+import Cart from './screens/cart';
 
 export default function App() {
+  const MainNavigator = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.textStyles}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <NavigationContainer>
+        <MainNavigator.Navigator screenOptions={{headerShown:false}} initialRouteName = "Login">
+          <MainNavigator.Screen name = "Home" component = {Home} />
+          <MainNavigator.Screen name = "Login" component = {Login} />
+        </MainNavigator.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -14,12 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#009900',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  textstyles: {
-    color: 'red',
-    fontSize: 100
-  }
 });
